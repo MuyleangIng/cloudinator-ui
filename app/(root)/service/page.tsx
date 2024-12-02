4300c4b945ec0ccb1aa828a9ba04ba3b9ca8be0c
VathanakSol <vathanaksol1605@gmail.com>
2024-12-02 14:42:29 +0700

    init route structure of landing page in home page

---
+++ app/(root)/service/page.tsx
@@ -0,0 +1,14 @@
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Service Page",
+    description: "Cloudinator Application",
+};
+
+export default function ServicePage() {
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <h1>This is Service Page</h1>
+        </div>
+    );
+}
